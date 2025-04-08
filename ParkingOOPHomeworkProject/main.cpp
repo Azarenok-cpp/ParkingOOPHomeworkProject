0d9a9fdce63635e596b9e0f2f90fcdeef78b3612
Rodion Azaronak <rodionazaronak@gmail.com>
2025-04-08 22:36:33 +0300

    Add pointers and heap using

--- ParkingOOPHomeworkProject/main.cpp
+++ ParkingOOPHomeworkProject/main.cpp
@@ -3,41 +3,41 @@
 #include "Security.h"
 
 int main() {
-	Car car1;
-
+	Car* car1 = new Car;
+	
 	cout << "Input brand of your car: ";
-	getline(cin, car1.brand);
+	getline(cin, car1->brand);
 
 	cout << "Input model of your car: ";
-	getline(cin, car1.model);
+	getline(cin, car1->model);
 
 	cout << "Input age of your car: ";
-	cin >> car1.age;
+	cin >> car1->age;
 
 	cout << "Input price of your car: ";
-	cin >> car1.price;
+	cin >> car1->price;
 
 	string unbrokenAnswer;
 	cout << "Is your car unbroken(y/n): ";
 	cin >> unbrokenAnswer;
-	car1.unbroken = unbrokenAnswer == "y";
+	car1->unbroken = unbrokenAnswer == "y";
 
 	cout << "Input storage time of your car (will be kept for ... days): ";
-	cin >> car1.storageTime;
+	cin >> car1->storageTime;
 	
 	cout << endl;
-	cout << car1.getInfo();
+	cout << car1->getInfo();
 
 	Security security;
+	//car gone away
+	security.letCarOut(car1);
 
-	security.letCarOut(&car1);
-
-	cout << car1.getInfo();
-
-	security.letCarIn(&car1);
+	cout << car1->getInfo();
+	//car came back
+	security.letCarIn(car1);
 	
-	cout << car1.getInfo();
+	cout << car1->getInfo();
 
-
+	delete car1;
 	return 0;
 }
